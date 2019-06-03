f5d1c80176c8c96c71e9a60e7edf4e7ca69b8a9f
alfarog1 <alfaro.geovanny@gmail.com>
2019-06-03 08:50:32 -0600

    final commit

--- PainTrackerREST/src/main/java/com/skilldistillery/paintracker/PainTrackerRestApplication.java
+++ PainTrackerREST/src/main/java/com/skilldistillery/paintracker/PainTrackerRestApplication.java
@@ -2,12 +2,21 @@ package com.skilldistillery.paintracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PainTrackerRestApplication {
+public class PainTrackerRestApplication extends SpringBootServletInitializer {
 
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(PainTrackerRestApplication.class);
+	  }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PainTrackerRestApplication.class, args);
 	}
 
 }
+
+
--- README.md
+++ README.md
@@ -15,3 +15,15 @@
 #### Purpose and usage of the Application
 This application is made for pain management. The purpose is to track pain. Not only location, but intensity, triggers that caused the pain, but also start and end points. This makes it easier to diagnose the recurring issues.
 
+How To Operate
+The app is located at http://52.26.41.223:8087/paintracker/api/
+
+API Routes
+
+* get all pains -> "pains"
+* get pains by range of intensit -> "pain/search/intensity/{low}/{high}"
+	
+* Save pain to the tracker -> "pain"
+* Find pain by ID -> "pain/{id}"
+* Delete pain	 -> "pain/{id}"	 
+* Update pain -> "pain/{id}")
